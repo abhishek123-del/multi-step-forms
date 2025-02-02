c6e5a41ecd7f01554dd16352d75d4b7159ca299b
Abhishek <abhishekchawariya25@gmail.com>
2025-02-02 06:40:11 -0800

    Personal info component modified

--- src/components/PersInfo.jsx
+++ src/components/PersInfo.jsx
@@ -2,9 +2,33 @@
 
 const PersInfo = () => {
     return (
-        <div className="col-lg-8 personalInfo">
-           
-        </div>
+        <>
+            <div className="col-lg-8 box">
+                <div className="personalInfo">
+                    <h1>Personal Info</h1>
+                    <p>Please provide your name,email address, and phone number.</p>
+                    <div className="form-group">
+                        <label htmlFor="">Name</label>
+                        <input type="text" name="" id="" placeholder="e.g Stephen King" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="">Email Address</label>
+                        <input type="text" name="" id="" placeholder="e.g stephenking@lorem.com" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="">Phone Number</label>
+                        <input type="text" name="" id="" placeholder="e.g +1 234 567 890" className="form-control" />
+                    </div>
+                </div>
+                <div className="bt-group">
+                    <div className="col-12">
+                        <button className="btn-sm btn-primary">Next step</button>
+                    </div>
+
+                </div>
+            </div>
+
+        </>
     )
 };
 export default PersInfo;
